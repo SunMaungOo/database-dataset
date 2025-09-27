0684148eb86107284d6952616750c328170b3f90
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2025-09-27 17:13:56 +0630

    fix function overload issue

--- src/connector.py
+++ src/connector.py
@@ -11,19 +11,19 @@ def get_dataset(connection_str:str,\
         return get_database_dataset(connection_str=connection_str,\
                                  host=host,\
                                 database=database,\
-                                query=MSSQL_GET_INFO_SQL)
+                                query_info=MSSQL_GET_INFO_SQL)
     elif DB_TYPE=="oracle":
         return get_database_dataset(connection_str=connection_str,\
                                     host=host,\
                                     database=database,\
-                                    query=ORACLE_GET_INFO_SQL)
+                                    query_info=ORACLE_GET_INFO_SQL)
 
     return None
     
 def get_database_dataset(connection_str:str,\
                       host:str,\
                       database:str,\
-                      query:str)->Optional[List[Dataset]]:
+                      query_info:str)->Optional[List[Dataset]]:
     
     grouped_column:Dict[str,List[Column]] = dict()
 
@@ -31,67 +31,7 @@ def get_database_dataset(connection_str:str,\
 
     try:
         for row in query(connection_str=connection_str,\
-                        query=query):
-            
-            schema_name = row[0]
-
-            object_name = row[1]
-
-            object_type = row[2]
-
-            column_name = row[3]
-
-            column_type = row[4]
-
-            key = f"{schema_name}.{object_name}.{object_type}"
-
-            if key not in grouped_column:
-                grouped_column[key] = list()
-
-            grouped_column[key].append(
-                Column(name=column_name,\
-                       type=column_type))
-            
-        for key in grouped_column:
-            
-            blocks = key.split(".")
-
-            schema_name = blocks[0]
-
-            object_name = blocks[1]
-
-            object_type = blocks[2]
-
-            datasets.append(
-                Dataset(
-                    host=host,\
-                    database=database,\
-                    schema=schema_name,\
-                    name=object_name,\
-                    type=object_type,\
-                    columns=grouped_column[key]
-                )
-            )
-
-
-        return datasets
-
-
-    except:
-        return None
-
-
-def get_database_dataset(connection_str:str,\
-                      host:str,\
-                      database:str)->Optional[List[Dataset]]:
-    
-    grouped_column:Dict[str,List[Column]] = dict()
-
-    datasets:List[Dataset] = list()
-
-    try:
-        for row in query(connection_str=connection_str,\
-                        query=MSSQL_GET_INFO_SQL):
+                        query=query_info):
             
             schema_name = row[0]
 
