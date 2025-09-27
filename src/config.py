5e8af20a14326786ebf1d38e00050d40d881e879
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2025-09-27 16:53:01 +0630

    add support for oracle database

--- src/config.py
+++ src/config.py
@@ -89,3 +89,72 @@ AND column_info.TABLE_NAME = table_info.TABLE_NAME
 
 """
 
+ORACLE_GET_INFO_SQL = """
+
+WITH column_info AS 
+(
+    SELECT OWNER AS SCHEMA_NAME,
+    TABLE_NAME,
+    COLUMN_NAME,
+    DATA_TYPE,
+    CASE
+        WHEN DATA_TYPE IN 
+        (
+            'CHAR',
+            'VARCHAR2',
+            'NCHAR',
+            'NVARCHAR2'
+        )
+        THEN
+            CASE 
+                WHEN CHAR_USED = 'C'
+                THEN '(' || CHAR_LENGTH || ')'
+                ELSE '(' || DATA_LENGTH || ')'
+            END
+        WHEN DATA_TYPE = 'RAW'
+        THEN '(' || DATA_LENGTH || ')'
+        WHEN DATA_TYPE = 'NUMBER'
+        THEN 
+            CASE
+                WHEN DATA_PRECISION IS NOT NULL 
+                THEN '(' || DATA_PRECISION || ',' || DATA_SCALE || ')'
+            END    
+    END AS DATA_TYPE_LENGTH,
+    CASE NULLABLE
+      WHEN 'Y'
+      THEN 'null'
+      ELSE 'not null'
+    END AS IS_NULLABLE 
+    FROM ALL_TAB_COLUMNS
+),table_info AS 
+(
+    SELECT OWNER AS SCHEMA_NAME,
+    OBJECT_NAME AS TABLE_NAME,
+    CASE OBJECT_TYPE
+        WHEN 'TABLE'
+        THEN 'table'
+        WHEN 'VIEW'
+        THEN 'view'
+    END AS TABLE_TYPE
+    FROM ALL_OBJECTS
+    WHERE OBJECT_TYPE IN 
+    (
+        'TABLE',
+        'VIEW'
+    )
+)
+SELECT column_info.SCHEMA_NAME,
+column_info.TABLE_NAME,
+table_info.TABLE_TYPE,
+column_info.COLUMN_NAME,
+CASE
+    WHEN column_info.DATA_TYPE_LENGTH IS NULL
+	THEN column_info.DATA_TYPE || ' ' || IS_NULLABLE
+	ELSE column_info.DATA_TYPE || DATA_TYPE_LENGTH || ' ' || IS_NULLABLE
+END AS COLUMN_DATA_TYPE
+FROM column_info
+INNER JOIN table_info
+ON column_info.SCHEMA_NAME = table_info.SCHEMA_NAME
+AND column_info.TABLE_NAME = table_info.TABLE_NAME
+
+"""
--- src/connector.py
+++ src/connector.py
@@ -1,6 +1,6 @@
 from model import Column,Dataset
 from typing import List,Optional,Dict
-from config import DB_TYPE,MSSQL_GET_INFO_SQL
+from config import DB_TYPE,MSSQL_GET_INFO_SQL,ORACLE_GET_INFO_SQL
 from database import query
 
 def get_dataset(connection_str:str,\
@@ -8,13 +8,80 @@ def get_dataset(connection_str:str,\
                 database:str)->Optional[List[Dataset]]:
 
     if DB_TYPE=="mssql":
-        return get_mssql_dataset(connection_str=connection_str,\
+        return get_database_dataset(connection_str=connection_str,\
                                  host=host,\
-                                database=database)
+                                database=database,\
+                                query=MSSQL_GET_INFO_SQL)
+    elif DB_TYPE=="oracle":
+        return get_database_dataset(connection_str=connection_str,\
+                                    host=host,\
+                                    database=database,\
+                                    query=ORACLE_GET_INFO_SQL)
 
     return None
     
-def get_mssql_dataset(connection_str:str,\
+def get_database_dataset(connection_str:str,\
+                      host:str,\
+                      database:str,\
+                      query:str)->Optional[List[Dataset]]:
+    
+    grouped_column:Dict[str,List[Column]] = dict()
+
+    datasets:List[Dataset] = list()
+
+    try:
+        for row in query(connection_str=connection_str,\
+                        query=query):
+            
+            schema_name = row[0]
+
+            object_name = row[1]
+
+            object_type = row[2]
+
+            column_name = row[3]
+
+            column_type = row[4]
+
+            key = f"{schema_name}.{object_name}.{object_type}"
+
+            if key not in grouped_column:
+                grouped_column[key] = list()
+
+            grouped_column[key].append(
+                Column(name=column_name,\
+                       type=column_type))
+            
+        for key in grouped_column:
+            
+            blocks = key.split(".")
+
+            schema_name = blocks[0]
+
+            object_name = blocks[1]
+
+            object_type = blocks[2]
+
+            datasets.append(
+                Dataset(
+                    host=host,\
+                    database=database,\
+                    schema=schema_name,\
+                    name=object_name,\
+                    type=object_type,\
+                    columns=grouped_column[key]
+                )
+            )
+
+
+        return datasets
+
+
+    except:
+        return None
+
+
+def get_database_dataset(connection_str:str,\
                       host:str,\
                       database:str)->Optional[List[Dataset]]:
     
